7421a699459e6b99bde0187e10ba2fb946349fce
noelRico <nrico.632apps@gmail.com>
2024-09-25 10:12:48 +0800

    upload

--- js/web.js
+++ js/web.js
@@ -1,126 +1,149 @@
-$(function() {
-	AOS.init();
+		const sections = document.querySelectorAll('section, div.c1, div.c2');
+        window.addEventListener('scroll', () => {
+            const triggerBottom = window.innerHeight / 1.5;
+            sections.forEach(section => {
+                const sectionTop = section.getBoundingClientRect().top;
+                if(sectionTop < triggerBottom) {
+                    section.classList.add('active');
+                } else {
+                    section.classList.remove('active');
+                }
+            });
+        });
 
-	$('#fosterfoodsweb').click(function() {
-		let imgs = ``;
-		for (var i = 12; i >= 1; i--) {
-			imgs += `
-				<div class="carousel-item ${i == 1 ? 'active' : ''}">
-					<a href="./imgs/fosterfoods${i}.png" target="_blank">
-						<img src="./imgs/fosterfoods${i}.png" class="border border-dark pointer d-block w-100">
-					</a>
-				</div>
-			`;
-		}
-		showGalleryModal('GALLERY', imgs, 500);
-	});
+        // Image gallery and modal functionality
+        const projectsImages = {
+            1: [	//stormaze
+                'imgs/projs/stormaze/web/0.jpg', 
+                'imgs/projs/stormaze/web/1.jpg',
+                'imgs/projs/stormaze/web/2.jpg',
+                'imgs/projs/stormaze/web/3.jpg',
+                'imgs/projs/stormaze/web/4.jpg',
+                'imgs/projs/stormaze/web/5.jpg',
+                'imgs/projs/stormaze/web/6.jpg',
+                'imgs/projs/stormaze/web/7.jpg',
+                'imgs/projs/stormaze/app/1.png',
+                'imgs/projs/stormaze/app/2.jpg',
+                'imgs/projs/stormaze/app/3.jpg',
+                'imgs/projs/stormaze/app/4.jpg',
+                'imgs/projs/stormaze/app/5.png',
+                'imgs/projs/stormaze/app/6.png',
+                'imgs/projs/stormaze/app/7.png',
+                'imgs/projs/stormaze/app/8.jpg',
+            ],
+            2: [	//PSI
+                'imgs/projs/psi/1.jpg',
+                'imgs/projs/psi/2.jpg',
+                'imgs/projs/psi/3.jpg',
+                'imgs/projs/psi/4.jpg',
+                'imgs/projs/psi/5.jpg',
+                'imgs/projs/psi/6.jpg',
+            ],
+            3: [	//PGA-DCM
+                'imgs/projs/dcm/1.png',
+                'imgs/projs/dcm/2.png',
+                'imgs/projs/dcm/3.png',
+                'imgs/projs/dcm/4.png',
+                'imgs/projs/dcm/5.png',
+                'imgs/projs/dcm/6.png',
+                'imgs/projs/dcm/7.png',
+                'imgs/projs/dcm/8.png',
+            ],
+	        4: [	//Exlog
+                'imgs/projs/exlog/1.jpg',
+                'imgs/projs/exlog/2.jpg',
+                'imgs/projs/exlog/3.jpg',
+                'imgs/projs/exlog/4.jpg',
+                'imgs/projs/exlog/5.jpg',
+                'imgs/projs/exlog/6.jpg',
+                'imgs/projs/exlog/7.jpg',
+                'imgs/projs/exlog/8.jpg',
+                'imgs/projs/exlog/9.jpg',
+                'imgs/projs/exlog/10.jpg',
+            ],
+	        5: [	//IDEM
+                'imgs/projs/idem/1.jpg',
+                'imgs/projs/idem/2.jpg',
+                'imgs/projs/idem/3.jpg',
+                'imgs/projs/idem/4.jpg',
+                'imgs/projs/idem/5.jpg',
+                'imgs/projs/idem/6.jpg',
+                'imgs/projs/idem/7.jpg',
+                'imgs/projs/idem/8.jpg',
+            ],
+	        6: [	//KPI
+                'imgs/projs/kpi/1.png',
+                'imgs/projs/kpi/2.png',
+                'imgs/projs/kpi/3.png',
+                'imgs/projs/kpi/4.png',
+                'imgs/projs/kpi/5.png',
+                'imgs/projs/kpi/6.png',
+            ],
+            7: [	//Surelog
+                'imgs/projs/surelog/1.jpg',
+                'imgs/projs/surelog/2.jpg',
+                'imgs/projs/surelog/3.jpg',
+                'imgs/projs/surelog/4.jpg',
+                'imgs/projs/surelog/5.jpg',
+                'imgs/projs/surelog/6.jpg',
+                'imgs/projs/surelog/7.jpg',
+                'imgs/projs/surelog/8.jpg',
+                'imgs/projs/surelog/9.jpg',
+            ],
+        };
 
-	$('#fosterfoodsmobile').click(function() {
-		let imgs = ``;
-		for (var i = 13; i >= 1; i--) {
-			imgs += `
-				<div class="carousel-item ${i == 1 ? 'active' : ''}">
-					<a href="./imgs/mobilefosterfoods${i}.jpg" target="_blank">
-						<img src="./imgs/mobilefosterfoods${i}.jpg" class="border border-dark pointer d-block w-100">
-					</a>
-				</div>
-			`;
-		}
-		showGalleryModal('GALLERY', imgs, 300);
-	});
+        const imageLength = [0,15,5,7,9,7,5,8];
+        selectedGallery = 0;
+        const modalImg = document.getElementById("modalImg");
+        currentImg = 0;
 
-	$('#surelogweb').click(function() {
-		let imgs = ``;
-		for (var i = 10; i >= 1; i--) {
-			imgs += `
-				<div class="carousel-item ${i == 1 ? 'active' : ''}">
-					<a href="./imgs/surelog${i}.png" target="_blank">
-						<img src="./imgs/surelog${i}.png" class="border border-dark pointer d-block w-100">
-					</a>
-				</div>
-			`;
-		}
-		showGalleryModal('GALLERY', imgs, 500);
-	});
+        function openGallery(projectId) {
+            const modal = document.getElementById("modal");
+            modal.style.display = "flex";
+            selectedGallery = projectId;
+            currentImg = 0;
+            // Load first image of project
+            modalImg.src = projectsImages[projectId][0];
 
-	$('#surelogmobile').click(function() {
-		let imgs = ``;
-		for (var i = 4; i >= 1; i--) {
-			imgs += `
-				<div class="carousel-item ${i == 1 ? 'active' : ''}">
-					<a href="./imgs/mobilesurelog${i}.jpg" target="_blank">
-						<img src="./imgs/mobilesurelog${i}.jpg" class="border border-dark pointer d-block w-100">
-					</a>
-				</div>
-			`;
-		}
-		showGalleryModal('GALLERY', imgs, 300);
-	});
+        }
 
-	$('#geogreenweb').click(function() {
-		let imgs = ``;
-		for (var i = 9; i >= 1; i--) {
-			imgs += `
-				<div class="carousel-item ${i == 1 ? 'active' : ''}">
-					<a href="./imgs/geogreen${i}.png" target="_blank">
-						<img src="./imgs/geogreen${i}.png" class="border border-dark pointer d-block w-100">
-					</a>
-				</div>
-			`;
-		}
-		showGalleryModal('GALLERY', imgs, 500);
-	});
+        function next(){
+            currentImg++;
+            mylength = imageLength[selectedGallery];
+            if(currentImg > mylength ){ currentImg = 0; }
+            modalImg.src = projectsImages[selectedGallery][currentImg];
+        }
 
-	$('#pgadcmweb').click(function() {
-		let imgs = ``;
-		for (var i = 8; i >= 1; i--) {
-			imgs += `
-				<div class="carousel-item ${i == 1 ? 'active' : ''}">
-					<a href="./imgs/dcm${i}.png" target="_blank">
-						<img src="./imgs/dcm${i}.png" class="border border-dark pointer d-block w-100">
-					</a>
-				</div>
-			`;
-		}
-		showGalleryModal('GALLERY', imgs, 500);
-	});
+        function back(){
+            currentImg--;
+            mylength = imageLength[selectedGallery];
+            if(currentImg < 0 ){ currentImg = mylength; }
+            modalImg.src = projectsImages[selectedGallery][currentImg];
+        }
 
-	$('#pgadcmmobile').click(function() {
-		let imgs = ``;
-		for (var i = 12; i >= 1; i--) {
-			imgs += `
-				<div class="carousel-item ${i == 1 ? 'active' : ''}">
-					<a href="./imgs/pgadcm${i}.png" target="_blank">
-						<img src="./imgs/pgadcm${i}.png" class="border border-dark pointer d-block w-100">
-					</a>
-				</div>
-			`;
-		}
-		showGalleryModal('GALLERY', imgs, 300);
-	});
+        function closeModal() {
+            document.getElementById("modal").style.display = "none";
+            const existingGallery = document.querySelector('.gallery');
+            if (existingGallery) {
+                existingGallery.remove();
+            }
+        }
 
-	function showGalleryModal(title, imgs, size) {
-		showModal(``,`
-			<span class="float-right">
-				<h4><i class="fa fa-times pointer" onclick="Swal.close()"></i></h4>
-			</span>
-			<br><br>
-			<h4>${title}</h4>
-			<br>
-			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-				<div class="carousel-inner">
-					${imgs}
-				</div>
-				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-				<br>
-			</div>
-		`, size)
-	}
-});
+        window.onload = function() {
+            var script = document.createElement("script");
+            script.src = "external-script.js";
+            document.body.appendChild(script);
+        };
+
+        if (window.innerWidth <= 768) {
+            // Mobile screen
+            document.querySelectorAll('pre').forEach(preTag => {
+                const pTag = document.createElement('p');
+                pTag.textContent = preTag.textContent;
+                pTag.classList.add('aboutTxt');
+                preTag.replaceWith(pTag);
+            });
+        } else {
+            // Desktop screen
+            console.log("Desktop device");
+        }
