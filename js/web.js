c2d1804aa7ee3d8519820408c72e678030b59ca5
noelRico <nrico.632apps@gmail.com>
2025-01-16 09:39:59 +0800

    update animation

--- js/web.js
+++ js/web.js
@@ -1,6 +1,6 @@
 		const sections = document.querySelectorAll('section, div.c1, div.c2');
         window.addEventListener('scroll', () => {
-            const triggerBottom = window.innerHeight / 1.5;
+            const triggerBottom = window.innerHeight / 1.3;
             sections.forEach(section => {
                 const sectionTop = section.getBoundingClientRect().top;
                 if(sectionTop < triggerBottom) {
